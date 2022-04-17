80a97cc9ef90567993839a00f7ee6d2ba1440724
sipmarch7 <sipmarch7@hotmail.com>
2022-04-17 12:38:01 +0300

    seperate script .js file from html v0.05

---
+++ src/script.js
@@ -0,0 +1,13 @@
+$s = jQuery.noConflict();
+
+const dataText = [
+    "Hi! My name is Spiros and I am from Greece.",
+    "I am currently finishing writing my thesis",
+]
+
+$s(document).ready(function () {
+    console.log("hi");
+
+
+
+})
