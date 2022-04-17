90528330c67917bf008934187a6bbe5de57da48e
Spyridon Peppas <sipmarch7@hotmail.com>
2022-04-17 17:15:03 +0300

    added touchscreen and mouse eventlistener - just woke up from nap - heading to starbucks to continue - v0.06

--- src/script.js
+++ src/script.js
@@ -1,13 +1,30 @@
-$s = jQuery.noConflict();
-
 const dataText = [
     "Hi! My name is Spiros and I am from Greece.",
     "I am currently finishing writing my thesis",
 ]
 
-$s(document).ready(function () {
-    console.log("hi");
+// const typeSpeed = 
+window.onload = () => {
+    if (is_touch_enabled()) {
+        console.log("touchscreen");
+        document.addEventListener("touchstart", (event) => {
+            playTime();
+        })
+    } else {
+        console.log("NOT touchscreen");
+        document.addEventListener('keyup', (event) => {
+            playTime();
+        })
+    }
+}
 
+const playTime = async () => {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    console.log("Play time!");
+}
 
-
-})
+function is_touch_enabled() {
+    return ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0);
+}
