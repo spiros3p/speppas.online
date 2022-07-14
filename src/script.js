85eabc778e049e5b10cf5c2aa9a87d6e92e9451a
sipmarch7 <sipmarch7@hotmail.com>
2022-07-14 12:37:23 +0300

    update bug

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ const initDataText = [
 const dataText = [
     "- My name is Spiros, I am 25 yo and I am living in Athens, Greece.",
     "- I graduated in July 2022 from the Electrical and Computer Engineering department of the University of Patras, Greece.",
-    "- For my thesis, I developed a GUI for configuring 5G base stations in private 5G networks. (Angular & nodeJs)",
+    "- For my thesis, I developed a GUI for configuring 5G base stations in private 5G networks. (Angular and nodeJs)",
     "- In the meantime, I worked full-time as a Web Developer at the NCSR Demokritos between July 2021 - June 2022.",
     "- Did I mention that I have also been trading NFTs on the WAX and FLOW blockchains, as a side hustle, for the past year?",
     "- Which has intrigued me into learning smart contracts development",
